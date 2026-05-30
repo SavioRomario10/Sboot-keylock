fa0b7050c360512c5105f685b00a99d2dc1a543d
Sávio Romário <savioromario10@gmail.com>
2026-05-30 16:38:03 -0300

    first commit

---
+++ src/test/java/sbootkeyclock/SbootKeyclockApplicationTests.java
@@ -0,0 +1,13 @@
+package sbootkeyclock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootKeyclockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
